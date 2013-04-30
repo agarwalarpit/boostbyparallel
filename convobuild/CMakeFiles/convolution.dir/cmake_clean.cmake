5389429163054690b2bac5ce5078f3bb59843c85
asquared <arpitagarwal2901@gmail.com>
2013-04-30 00:24:07 -0500

    Convolution running successfully

---
+++ convobuild/CMakeFiles/convolution.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/convolution.dir/Convolution.cpp.o"
+  "convolution.pdb"
+  "convolution"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/convolution.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
